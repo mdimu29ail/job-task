bfb04f106c683055bbd9a85666c836c3ebf96d92
Imamul <mdimu29@gmail.com>
2025-10-29 01:15:59 +0600

    add key

--- libs/supabase-client.ts
+++ libs/supabase-client.ts
@@ -2,10 +2,9 @@ import { createClient } from '@supabase/supabase-js';
 
 // ✅ Define your Supabase credentials safely
 const supabaseUrl =
-  process.env.NEXT_PUBLIC_SUPABASE_URL ||
-  'https://ahlryolxsxvzqvkrsyph.supabase.co';
+  process.env.SUPABASE_URL || 'https://ahlryolxsxvzqvkrsyph.supabase.co';
 const supabaseAnonKey =
-  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY ||
+  process.env.SUPABASE_ANON_KEY ||
   'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImFobHJ5b2x4c3h2enF2a3JzeXBoIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjE0MDM5NDgsImV4cCI6MjA3Njk3OTk0OH0.P0DTCLGlFmtxCj20yKPv4AxLBkw26kq7cOAfBQuOX6g';
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
